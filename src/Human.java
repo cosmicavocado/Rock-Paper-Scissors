3e2eb0b28cab871e6cf06efa97bd6910e6e8352b
Mariah Legg <mariah.legg@gmail.com>
2021-12-10 15:14:34 -0500

    Human with constructor and getChoice

---
+++ src/Human.java
@@ -0,0 +1,27 @@
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Human extends Player{
+
+    Human(String player) {
+        super(player);
+    }
+
+    @Override
+    public int getChoice() {
+        int input = 0;
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+
+        if (str.equals("1") || str.equals("2") || str.equals("3") && !str.isEmpty()) {
+            input = Integer.parseInt(str);
+        } else if (str.equals("q")) {
+            GameState.exit();
+        }
+        else {
+            System.out.println("Please enter valid input.");
+        }
+        return input;
+    }
+
+}
