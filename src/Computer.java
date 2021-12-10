13135e6d78be9f87efc502b54436304aedfe5f78
Mariah Legg <mariah.legg@gmail.com>
2021-12-10 15:10:14 -0500

    Computer class complete with Constructor and getChoice to generate computer choice

--- src/Computer.java
+++ src/Computer.java
@@ -1,12 +1,15 @@
 public class Computer extends Player{
 
+    public Computer(String player) {
+        super(player);
+    }
+
     public Computer() {
         super("Computer");
     }
 
     @Override
     public int getChoice() {
-        String[] choices = {"rock", "paper", "scissors"};
         int choice = (int) (Math.random() * choices.length);
         return choice;
     }
