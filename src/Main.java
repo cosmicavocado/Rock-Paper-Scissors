5a7d0b1c5dc5318a8bb8308eb21d4b5785e9ffbb
Mariah Legg <mariah.legg@gmail.com>
2021-12-10 15:34:30 -0500

    Main can perform basic game loop

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
+        Human player1 = new Human("player1");
+        GameState game = new GameState();
 
+        // Menu
+        game.mainMenu();
+        Optional<Integer> choice = Optional.ofNullable(player1.getChoice());
+        game.setMode(choice);
 
+        // Create Game of Desired Game Mode
+        Object player2 = game.createGame(choice, player1);
     }
 }
